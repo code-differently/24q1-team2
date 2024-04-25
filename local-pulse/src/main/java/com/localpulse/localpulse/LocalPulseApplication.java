83a4ff3d577d7a05c6f81b258ad67d4984b1c64a
RichHawkins <richhawkins1986@gmail.com>
2024-04-25 11:10:53 -0400

    feat: adds Spring Boot

---
+++ local-pulse/src/main/java/com/localpulse/localpulse/LocalPulseApplication.java
@@ -0,0 +1,13 @@
+package com.localpulse.localpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalPulseApplication.class, args);
+	}
+
+}
